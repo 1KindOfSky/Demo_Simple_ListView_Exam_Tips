6d5b7c6c229337cc3b3dfb1bcb0daeb633ce3220
1KindOfSky <20029443@myrp.edu.sg>
2021-06-09 14:31:34 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id20029443/demosimplelistviewexamtips/MainActivity.java
@@ -0,0 +1,33 @@
+package sg.edu.rp.c346.id20029443.demosimplelistviewexamtips;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lvExamTips;
+    String[] newArray;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lvExamTips = findViewById(R.id.listViewExamTips);
+
+        String[] newArray = new String [5];
+
+        newArray[0] = "Don't just read the code, code it as much as possible during each practical session";
+        newArray[1] = "Seek help from the lecturer ASAP, don't wait till you lost in the jungle";
+        newArray[2] = "Prepare your template source code for each topic";
+        newArray[3] = "Create a few empty Android projects to speed up your coding during the exam";
+        newArray[4] = "Ensure that your Android Studio is up and running before the exam";
+
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, newArray);
+
+        lvExamTips.setAdapter(adapter);
+    }
+}
